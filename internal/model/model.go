3805229ca1e8f7e5c8270850f77381924918f3cc
Joshua Baldwin <Joshua.L.Baldwin@centene.com>
2020-05-17 00:44:07 -0500

    added models for movies and tv shows

---
+++ internal/model/model.go
@@ -0,0 +1,36 @@
+package model
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+//Show model
+type Show struct {
+	ID *primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+
+	Name      string    `bson:"name,omitempty" json:"name,omitempty"`
+	Genres    []string  `bson:"genres,omitempty" json:"genres,omitempty"`
+	Platforms []string  `bson:"platforms,omitempty" json:"platforms,omitempty"`
+	Runtime   string    `bson:"runtime,omitempty" json:"runtime,omitempty"`
+	Created   time.Time `bson:"created,omitempty" json:"created,omitempty"`
+	Updated   time.Time `bson:"updated,omitempty" json:"updated,omitempty"`
+	Watched   bool      `bson:"watched,omitempty" json:"watched,omitempty"`
+}
+
+//Movie model
+type Movie struct {
+	Show `bson:",inline"`
+
+	Franchise string `bson:"franchise,omitempty" json:"franchise,omitempty"`
+}
+
+//TV model
+type TV struct {
+	Show `bson:",inline"`
+
+	Review  string `bson:"review,omitempty" json:"review,omitempty"`
+	Airing  bool   `bson:"airing,omitempty" json:"airing,omitempty"`
+	Seasons int    `bson:"seasons,omitempty" json:"seasons,omitempty"`
+}
