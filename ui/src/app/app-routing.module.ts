9cfdb3fb6c99636d8eee2e838cb0b7dbde6b9973
Joshua Baldwin <Joshua.L.Baldwin@centene.com>
2020-05-26 11:12:41 -0500

    adding basic components to begin movie page

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AppComponent } from '../app/app.component';
+import { MovieComponent } from './movie/movie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: AppComponent },
+  { path: 'movie', component: MovieComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecentComponent } from './recent/recent.component';
+import { StatsComponent } from './stats/stats.component';
+
+import { MovieService } from './movie.service';
+import { MovieComponent } from './movie/movie.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecentComponent,
+    StatsComponent,
+    MovieComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [MovieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
