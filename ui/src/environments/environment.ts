cd1ca469a2b13ea359d5f328cb4af76bcdc17f37
jobaldw <jobaldw@gmail.com>
2020-05-28 09:23:42 -0500

    creating weather service, adding env vars, and retreiving OWM api key

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '7c8174fd732efc1fbdbf1eef9905883a',
+  apiUrl: 'http://api.openweathermap.org/data/2.5'
 };
 
 /*
