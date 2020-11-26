c0b1c3f8d3402aec6d13dfeff4d479b96fce469d
Joshua L. Baldwin <jobaldw@Karlies-MBP.local>
2020-11-25 22:04:43 -0600

    updated env vars, moved weather service

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
+// TODO: update values
 export const environment = {
-  production: true
+  production: true,
+
+  // weatherAPI
+  apiKey: '7c8174fd732efc1fbdbf1eef9905883a', // https://home.openweathermap.org/api_keys
+  apiUrl: 'http://api.openweathermap.org/data/2.5',
+
+  //news-api
+  newsAPIURL: 'http://localhost:3001'
 };
