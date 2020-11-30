2bdc53f7f3850772dbb3e89647d89f27a94dc42e
Joshua L. Baldwin <jobaldw@Karlies-MBP.local>
2020-11-30 06:54:14 -0600

    updated helper API urls

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -2,10 +2,16 @@
 export const environment = {
   production: true,
 
-  // weatherAPI
+  //openWeatherAPI
   apiKey: '7c8174fd732efc1fbdbf1eef9905883a', // https://home.openweathermap.org/api_keys
   apiUrl: 'http://api.openweathermap.org/data/2.5',
 
-  //news-api
-  newsAPIURL: 'http://localhost:3001'
+  //my-calendar-api
+  calendarAPIURL: 'http://localhost:3000',
+
+  //my-entertainment-api
+  entertainmentAPIURL: 'http://localhost:3001',
+
+  //my-news-api
+  newsAPIURL: 'http://localhost:3002'
 };
--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -4,11 +4,19 @@
 
 export const environment = {
   production: false,
+  
+  //openWeatherAPI
   apiKey: '7c8174fd732efc1fbdbf1eef9905883a', // https://home.openweathermap.org/api_keys
   apiUrl: 'http://api.openweathermap.org/data/2.5',
 
-  //news-api
-  newsAPIURL: 'http://localhost:3001'
+  //my-calendar-api
+  calendarAPIURL: 'http://localhost:3000',
+
+  //my-entertainment-api
+  entertainmentAPIURL: 'http://localhost:3001',
+
+  //my-news-api
+  newsAPIURL: 'http://localhost:3002'
 };
 
 /*
